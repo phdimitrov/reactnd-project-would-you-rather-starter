0724821387cc86ea47e8ec48e39a7ac71c98836b
Philip Dimitrov <ph.dimitrov@gmail.com>
2019-03-30 22:13:27 +0200

    initial setup
    npm add react-icons
    npm add react-redux redux
    npm add redux-thunk
    npm add react-redux-loading
    npm add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {handleInitialData} from "../actions/shared";
+import {connect} from 'react-redux'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import LoadingBar from 'react-redux-loading';
+import SignIn from './SignIn';
 
 class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleInitialData());
+    }
+
     render() {
         return (
-            <div>
-                Start
-            </div>
+            <Router>
+                <Fragment>
+                    <LoadingBar/>
+                    <div className='container'>
+                        <Route path='/' exact component={SignIn}/>
+                    </div>
+                </Fragment>
+            </Router>
         )
     }
 }
 
-export default App;
+export default connect()(App);
---
+++ src/components/SignIn.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import { FaQuestion } from 'react-icons/fa/index';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div>
+                SignIn
+                <FaQuestion />
+            </div>
+        );
+    }
+}
+
+export default SignIn;
